b92dd75c3fbda0dbffecd7e3cf0ea3501c779bd1
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-02-13 22:04:38 +0300

    e-commerce site finished

--- 6-)hoc,context/src/main.jsx
+++ 6-)hoc,context/src/main.jsx
@@ -4,15 +4,19 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+
 import { ProductProvider } from "./context/productContext.jsx";
 import { BasketProvider } from "./context/basketContext.jsx";
-// import { UserProvider } from "./context/userContext.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <BasketProvider>
     <ProductProvider>
       <StrictMode>
         <App />
+
+        <ToastContainer />
       </StrictMode>
     </ProductProvider>
   </BasketProvider>
