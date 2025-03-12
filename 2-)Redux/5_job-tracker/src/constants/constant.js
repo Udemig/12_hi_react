d0160c4a3aecfe2943c247603e5c5ccef897a989
yusufyaman07 <yamanyusuf00000@gmail.com>
2025-03-13 00:14:25 +0300

    job Tracker interface creation, job adding, deleting features

---
+++ 2-)Redux/5_job-tracker/src/constants/constant.js
@@ -0,0 +1,5 @@
+const statusOptions = ["Mülakat", "Devam Ediyor", "Reddedildi"];
+
+const typeOptions = ["Tam Zamanlı", "Yarı Zamanlı", "Uzaktan", "Staj"];
+
+export { statusOptions, typeOptions };
---
+++ 2-)Redux/5_job-tracker/src/pages/Home/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Loader from "../../components/Loader";
+import Error from "../../components/Error";
+import Card from "../Create/Card";
+import "./home.scss";
+import Filter from "./Filter";
+const Home = () => {
+  // Store'a abone ol ve jobs verisini console'a yazdır
+  const { isLoading, error, jobs } = useSelector((store) => store.jobReducer);
+
+  return (
+    <div className="home-page">
+      {/*  Filter */}
+      <Filter />
+
+      {/* Jobs Data */}
+
+      {/* Yükleniyorsa Loader'ı Hata varsa Error bileşenini İş verileri geldiyse  render et */}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Error info={error} />
+      ) : (
+        <div className="cards-wrapper">
+          {jobs.map((job) => (
+            <Card key={job.id} job={job} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
